7aae82f44b5f7fca23456094dff60dfae6024bc9
joyogbu <obyjoy@yahoo.com>
2023-07-13 11:01:24 +0000

    task 0

---
+++ 0-uniq_users.sql
@@ -0,0 +1,4 @@
+/* SQL script that creates a table users following these requirements */
+
+-- create table if not exist
+CREATE TABLE IF NOT EXISTS users(id INT NOT NULL AUTO_INCREMENT PRIMARY KEY, email VARCHAR(255) NOT NULL UNIQUE, name VARCHAR(255))
