5a0b4ac100aae65c011a308664ee3524f0e6abc5
joyogbu <obyjoy@yahoo.com>
2023-07-14 20:48:19 +0000

    edited 13

--- 0x00-MySQL_Advanced/101-average_weighted_score.sql
+++ 0x00-MySQL_Advanced/101-average_weighted_score.sql
@@ -7,8 +7,8 @@ BEGIN
 	DECLARE count_user INT DEFAULT 0;
 	DECLARE i INT DEFAULT 0;
 	SELECT COUNT(*) FROM users INTO count_user;
-	SET i = 0;
-	WHILE i < count_user DO
+	SET i = 1;
+	WHILE i <= count_user DO
 		UPDATE users SET average_score = (SELECT SUM(score * weight) / SUM(weight) FROM corrections JOIN projects ON corrections.project_id = projects.id WHERE user_id = i) WHERE id = i;
 		SET i = i + 1;
 	END WHILE;
