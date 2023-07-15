2f4e19ee2164da6895df28ad88c9be5d2bf18738
joyogbu <obyjoy@yahoo.com>
2023-07-15 14:55:13 +0000

    task 6

--- 0x00-MySQL_Advanced/6-bonus.sql
+++ 0x00-MySQL_Advanced/6-bonus.sql
@@ -4,9 +4,9 @@
 DELIMITER $$
 CREATE PROCEDURE AddBonus(IN user_id INT, IN project_name VARCHAR(50), IN score INT)
 BEGIN
-	DECLARE my_prod_id INT DEFAULT 0;
+	DECLARE my_prod_id INT;
 	SELECT id INTO my_prod_id FROM projects WHERE name = project_name;
-	IF @my_prod_id = 0 THEN
+	IF my_prod_id = NULL THEN
 		INSERT INTO projects(name) VALUES(project_name);
 		SET my_prod_id = NEW.id;
 	END IF;
