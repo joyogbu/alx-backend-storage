be115eb183ae0812c0d169e3ec84fccb564fea3d
joyogbu <obyjoy@yahoo.com>
2023-07-16 00:21:37 +0000

    task 5

--- 0x00-MySQL_Advanced/7-average_score.sql
+++ 0x00-MySQL_Advanced/7-average_score.sql
@@ -4,8 +4,8 @@
 DELIMITER $$
 CREATE PROCEDURE ComputeAverageScoreForUser(IN user_id INT)
 BEGIN
-	DECLARE no_record INT;
-	SELECT COUNT(*) INTO no_record FROM corrections WHERE user_id = user_id;
-	UPDATE users SET average_score = (SELECT SUM(score) / no_record  FROM corrections WHERE user_id = user_id) WHERE id = user_id;
+	-- DECLARE no_record INT;
+	-- SELECT COUNT(*) INTO no_record FROM corrections WHERE user_id = user_id;
+	UPDATE users SET average_score = (SELECT AVG(score) FROM corrections WHERE user_id = user_id) WHERE id = user_id;
 END$$
 DELIMITER ;
