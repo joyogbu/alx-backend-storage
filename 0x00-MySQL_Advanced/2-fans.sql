32b48ff419e53cd14730977b5481edb4fc2639a3
joyogbu <obyjoy@yahoo.com>
2023-07-16 01:02:51 +0000

    task 8

--- 0x00-MySQL_Advanced/2-fans.sql
+++ 0x00-MySQL_Advanced/2-fans.sql
@@ -1,4 +1,4 @@
 /* SQL script that ranks country origins of bands, ordered by the number of (non-unique) fans */
 
 --rank country by number of fans
-SELECT origin, nb_fans GROUP BY
+SELECT origin, fans AS nb_fans GROUP BYorigin;
---
+++ 0x00-MySQL_Advanced/8-index_my_names.sql
@@ -0,0 +1,5 @@
+/* : 0.0%)
+Write a SQL script that creates an index idx_name_first on the table names and the first letter of name */
+
+-- create an index
+CREATE INDEX idx_name_first ON names (names);
