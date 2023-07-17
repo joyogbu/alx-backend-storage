7ccac4c1d0866c3937a0f7e6dfe0312821b000b6
joyogbu <obyjoy@yahoo.com>
2023-07-17 10:41:43 +0000

    task 11

--- 0x00-MySQL_Advanced/11-need_meeting.sql
+++ 0x00-MySQL_Advanced/11-need_meeting.sql
@@ -1,5 +1,6 @@
 /* script that creates a view need_meeting that lists all students that have a score under 80 (strict) and no last_meeting or more than 1 month */
 
 -- Create view
+--DROP VIEW IF EXISTS need_meeting;
 CREATE VIEW need_meeting AS
 SELECT name FROM students WHERE score < 80 AND last_meeting IS NULL;
