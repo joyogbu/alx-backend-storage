03fa2dc601c7f1e7ce3e73a5d76fbd450a7b65af
joyogbu <obyjoy@yahoo.com>
2023-07-17 13:18:09 +0000

    task 9

--- 0x00-MySQL_Advanced/9-index_name_score.sql
+++ 0x00-MySQL_Advanced/9-index_name_score.sql
@@ -1,4 +1,4 @@
 /* Script that creates an index idx_name_first_score on the table names and the first letter of name and score */
 
 --create index
-CREATE INDEX idx_name_first_score ON names (name(1), score);
+CREATE INDEX idx_name_first_score ON names (name(1) + score);
