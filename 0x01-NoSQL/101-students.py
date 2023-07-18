4f1ef1c97a0f0a813121a388d69ed7b19653874f
joyogbu <obyjoy@yahoo.com>
2023-07-18 15:23:09 -0700

    task 8 edited

---
+++ 0x01-NoSQL/101-students.py
@@ -0,0 +1,7 @@
+#!/usr/bin/env python3
+'''function that returns all students sorted by average score'''
+
+
+if __name__ == "__main__":
+    def top_students(mongo_collection):
+        
--- 0x01-NoSQL/8-all.py
+++ 0x01-NoSQL/8-all.py
@@ -8,9 +8,12 @@ from pymongo import MongoClient
 
 #client = MongoClient()
 
-if __name__ == "__main__":
+#if __name__ == "__main__":
 def list_all(mongo_collection):
     '''defining the function'''
-    client = MongoClient()
-    res = client.mongo_collection.find()
-    return res
+    if __name__ == "__main__":
+        my_list = []
+        client = MongoClient()
+        for res in client.mongo_collection.find():
+            my_list.append(res)
+            return (my_list)
