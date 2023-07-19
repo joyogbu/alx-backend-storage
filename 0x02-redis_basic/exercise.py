d47aed8da014559deaa57c9f49bf403b0aee6fdd
joyogbu <obyjoy@yahoo.com>
2023-07-19 15:40:51 +0000

    task 0

--- 0x02-redis_basic/exercise.py
+++ 0x02-redis_basic/exercise.py
@@ -4,19 +4,20 @@
 
 import uuid
 import redis
+from typing import Any
 
 
 class Cache:
     '''class for cache'''
 
-    def __init__(self):
+    def __init__(self) -> None:
         '''initializing the class'''
         self._redis = redis.Redis()
         self._redis.flushdb()
 
-    def store(self, data) -> str:
+    def store(self, data: Any) -> str:
         '''defining the store function'''
         random_key = uuid.uuid4()
-        str_key = str(random_key)
+        str_key: str = str(random_key)
         self._redis.set(str_key, data)
         return (str_key)
