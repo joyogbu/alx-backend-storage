4a83a64d6fd55e93dc508dccb89ab97709635f06
joyogbu <obyjoy@yahoo.com>
2023-07-20 03:12:14 +0000

    task 2

--- 0x02-redis_basic/exercise.py
+++ 0x02-redis_basic/exercise.py
@@ -21,3 +21,19 @@ class Cache:
         str_key: str = str(random_key)
         self._redis.set(str_key, data)
         return (str_key)
+
+    def get(self, key: str, fn: Optional[Callable]):
+        '''defining the function'''
+        if key not in self._redis:
+            return None
+        res = self._redis.get(key)
+        return (res)
+
+    def get_str(self):
+        '''defining the function'''
+        res_str = str(self.get())
+
+    def get_int(self):
+        '''defining the function'''
+        re = int(self.get())
+        return (re)
